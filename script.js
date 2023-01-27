5270d94d68fef428005f0871b63ca35a474ba578
David Zamora <david.zamora@correounivalle.edu.co>
2023-01-27 10:56:06 -0500

    archivo csv por comuna

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Crear mapa
-var map = L.map("map").setView([3.4372,-76.5225], 12);
+var map = L.map("map").setView([3.4372,-76.5225], 11.5);
 
 // Añadir capa de mapa satelital de Mapbox
 var mapboxSatelital = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -10,3 +10,62 @@ var mapboxSatelital = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZHZ6YW1vcmEiLCJhIjoiY2tteDZrbmprMG0ybTJ2azZtMWNnZmxudCJ9.RY8vFhfWklg9xmmebG98FA'
 }).addTo(map);
+
+
+// GRÁFICAS DASHBOARD
+// BARRAS
+
+// Seleccionar el elemento donde se quiere dibujar el gráfico
+var barChart = d3.select("#bar-chart");
+
+// Establecer el tamaño del gráfico
+var width = barChart.node().getBoundingClientRect().width;
+var height = 400;
+
+// Crear un svg dentro del elemento seleccionado
+var svg = barChart.append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Cargar el archivo csv
+var url = "https://gist.githubusercontent.com/DavidZamoraR/482c2e5d8880b128e063e17ab654a245/raw/d918e804c47c069d8f42c19de2fc67c65145a37a/Elec2019-Cali-CONCEJO.csv"
+d3.csv(url,{delimiter: ","}, function(error, data) {
+    var votes = data.map(function(d) { return d.votos; });
+    var parties = data.map(function(d) { return d.partido; });
+
+    console.log(data);
+
+
+    // Escala de barras
+    var x = d3.scaleBand()
+        .domain(parties)
+        .bandwidth([0, width])
+        .padding(0.1);
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(votes)])
+        .range([height, 0]);
+
+    // Crear las barras
+    svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.partido); })
+        .attr("y", function(d) { return y(d.votos); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.votos); })
+        .attr("fill", "steelblue");
+
+    // Agregar ejes
+    var xAxis = d3.axisBottom(x);
+    var yAxis = d3.axisLeft(y);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+});
